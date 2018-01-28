1275c1d9ff478ea5ea168e2a6fe12ea01991f056
aku181 <akshaya81@gmail.com>
2018-01-28 11:56:56 -0500

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Akshey Saluja on 1/28/18.
+//  Copyright © 2018 Akshey Saluja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
