6d3e5538a6cca72ddfd56fb02259e5312ea22fec
aku181 <akshaya81@gmail.com>
2018-01-28 18:58:02 -0500

    added filpcard funct.

--- Project 1/ViewController.swift
+++ Project 1/ViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func touchCard(_ sender: UIButton) {
-        
+        flipCard(withEmoji: "😃", on: sender)
     }
     func flipCard(withEmoji emoji:String, on button:UIButton)  {
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9313876295, green: 0.5386387758, blue: 0.00827854702, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
         
     }
   
